25b61f524a9dacdc0ad15936a7f0df2ba9bf0ddd
belolarboutique-byte <belolarboutique@gmail.com>
2025-12-02 03:49:46 -0300

    Update Navbar link from 'Produtos' to 'pagina-antiga'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
 
   const navLinks = [
     { to: "/", label: "Início" },
-    { to: "/#produtos", label: "Produtos" },
+    { to: "/#produtos", label: "pagina-antiga" },
     { to: "/faq", label: "Dúvidas" },
     { to: "/suporte", label: "Suporte" },
   ];
